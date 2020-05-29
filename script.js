65b17ebd0f5eb69bf1b0232dab04e2d22339d2b7
DariaKushniruk <darmiomail@gmail.com>
2020-05-29 17:10:23 +0300

    test2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict'
 console.log(typeof(String(null)));
 let ans = confirm("Are you sure?");
-console.log(ans);
+//console.log(ans);
 let ans2 = prompt("уоцл улоцу","ДА");
 console.log(ans2);
