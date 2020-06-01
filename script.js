b79b6d30f824ee0a5b5505700ec2032a125ba7c3
Daria <darmio@ukr.net>
2020-06-01 10:57:48 +0300

    task 1

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 'use strict'
-console.log(typeof(String(null)));
-let ans = confirm("Are you sure?");
-//console.log(ans);
-let ans2 = prompt("уоцл улоцу","ДА");
-console.log(ans2);
+
+let money = prompt("You budget for a momth?");
+let time  = prompt("Input date in format YYYY-MM-DD");
+let expensItem = prompt("Input expens item");
+let expensCost = prompt("Input cost of expenses");
+
+let appData  = {
+    budget:money,
+    timeDate:time,
+    expenses:{expensItem:expensCost},
+    optionalExpenses:{},
+    income:{},
+    savings:false
+
+}
+
+alert("Budget for a day is " + appData.budget/30);
