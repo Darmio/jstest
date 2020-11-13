1935939a8c37f6f3d768cc3d6d1f3bf5021b7e07
Daria <darmio@ukr.net>
2020-11-13 12:12:53 +0200

    2020_11_13

--- script.js
+++ script.js
@@ -10,73 +10,80 @@ function start(){
 }
 
 start();
-
+//ebugger;
 let appData  = {
     budget:money,
     timeDate:time,
     expenses:{},
     optionalExpenses:{},
     income:[],
-    savings:true
-
-}
-
-function chooseExpenses() {
-for(let i=0; i<2; i++){
-    let expensItem = prompt("Input expens item",''),
-        expensCost = prompt("Input cost of expenses",'');
-        if((typeof(expensItem))==='string' && (typeof(expensItem))!=null && (typeof(expensCost))!=null 
-           && expensItem!='' && expensCost!='' && expensItem.length<50) {
-               console.log('done');
-               appData.expenses[expensItem] = expensCost;
-           } else {
-               i--;
-           }
-}
-}
-chooseExpenses();
-
-function chooseOptExpenses(){
-    for(let i=0; i<3; i++){
-        let nameExpense = prompt("Input optional expense:");
-        if(typeof(nameExpense)==='string' && (typeof(nameExpense))!=null 
-            && nameExpense!='' && nameExpense.length<50){
-                appData.optionalExpenses[i+1]=nameExpense;
-            }
+    savings:true,
+    chooseExpenses:function(){
+        for(let i=0; i<2; i++){
+            let expensItem = prompt("Input expens item",''),
+                expensCost = prompt("Input cost of expenses",'');
+                if((typeof(expensItem))==='string' && (typeof(expensItem))!=null && (typeof(expensCost))!=null 
+                   && expensItem!='' && expensCost!='' && expensItem.length<50) {
+                       console.log('done');
+                       appData.expenses[expensItem] = expensCost;
+                   } else {
+                       i--;
+                   }
+        }
+    },
+    chooseOptExpenses:function(){
+        for(let i=0; i<3; i++){
+            let nameExpense = prompt("Input optional expense:");
+            if(typeof(nameExpense)==='string' && (typeof(nameExpense))!=null 
+                && nameExpense!='' && nameExpense.length<50){
+                    appData.optionalExpenses[i+1]=nameExpense;
+                }
+        }
+    },
+    detectDayBudget:function(){
+        appData.moneyPerDay = (appData.budget/30).toFixed();
+        alert("Budget for a day is " + appData.moneyPerDay);
+    },
+    detectLevel : function(){
+        if(appData.moneyPerDay<100){
+            console.log("Min wealth");
+        } else if(appData.moneyPerDay >=100 && appData.moneyPerDay <2000){
+            console.log("middle");
+        } else if(appData.log >= 2000) {
+            console.log("Hight");
+        
+        } else {
+            console.log("error input");
+        }
+    },
+    checkSavings : function(){
+        if(appData.savings == true){
+            let save = +prompt("Какова сумма накоплений"),
+                percent = +prompt("Под какой процент");
+            appData.monthIncome = save/100/12*percent;
+            alert("Income in month:" + appData.monthIncome);
+        }
+    },
+    chooseIncome : function() {
+        let item = prompt("Что принесет доп доход? (перечислите через запятую)", '');
+        appData.income = item.split(', ');
+        let dopInc;
+        do {
+            dopInc = prompt("еще доп");
+        }while(typeof(dopInc)!=='string' || dopInc=='' || dopInc==null)
+        appData.income.push(dopInc);
+        appData.income.sort();
     }
 }
-chooseOptExpenses();
 
-function detectDayBudget(){
-    appData.moneyPerDay = (appData.budget/30).toFixed();
-    alert("Budget for a day is " + appData.moneyPerDay);
+appData.chooseIncome();
+console.log(appData.income.forEach(function(item,i,arr){
+    alert((i+1)+'='+item+'(mas:'+arr+')');
+} ));
+
+for(let key in appData){
+    console.log(key+'='+appData[key]);
 }
-
-function detectLevel(){
-    if(appData.moneyPerDay<100){
-        console.log("Min wealth");
-    } else if(appData.moneyPerDay >=100 && appData.moneyPerDay <2000){
-        console.log("middle");
-    } else if(appData.log >= 2000) {
-        console.log("Hight");
-    
-    } else {
-        console.log("error input");
-    }
-}
-
-function checkSavings(){
-    if(appData.savings == true){
-        let save = +prompt("Какова сумма накоплений"),
-            percent = +prompt("Под какой процент");
-        appData.monthIncome = save/100/12*percent;
-        alert("Income in month:" + appData.monthIncome);
-    }
-}
-
-detectDayBudget();
-detectLevel();
-checkSavings();
  /// example circuit
 let c = 4
 function addX(x) {
