aba82b18435e28b4fb107d5f68ef141be019dc4f
Daria <darmio@ukr.net>
2021-09-16 13:33:56 +0300

    XMLReaquest

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -59,5 +59,48 @@ window.addEventListener('DOMContentLoaded', function(){
        }); 
     });
 
-        
+    
+    //FORM
+    let message = {
+        loading:"Loading...",
+        success:"Thanks, all finished OK",
+        failure:"Something error"
+
+    };
+
+    let form = document.querySelector('.main-form'),
+    input = form.getElementsByTagName('input'),
+    statusMessage = document.createElement('div');
+    statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(ev){
+       ev.preventDefault();
+       form.appendChild(statusMessage);
+       let request = new XMLHttpRequest();
+       request.open('GET','server.php');
+       //request.setRequestHeader('Content-type','application-x-www-form-urlencoded');
+       request.setRequestHeader('Content-type','application/json; charset=utf-8;');
+
+       let formData = new FormData(form);
+       let obj = {};
+       formData.forEach(function(value,key){ obj[key]=value; });
+       let json = JSON.stringify(obj); 
+       //request.send(formData);
+       request.send(json);
+
+       request.addEventListener('readystatechange', function () {
+          if(request.readyState<4){
+              statusMessage.innerHTML = message.loading;
+          }else if(request.readyState===4 && request.status==200){
+              statusMessage.innerHTML = message.success;   
+          }else statusMessage.innerHTML = message.failure;
+       })
+
+       for(let i =0; i<input.length; i++){
+           input[i].value='';
+       }
+
+    })
+    
+
 });
